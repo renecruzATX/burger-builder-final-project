caeb06847546bbbce1caa553d5d1c8a9d706e5cc
Rene Cruz <renecruzATX@outlook.com>
2018-12-08 09:28:23 -0600

    Forked burger builder for final project

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://user0:password0@ds127954.mlab.com:27954/burger-builder-final-project");
+
+
+const OrderRoutes = require("./OrderRoutes");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/orders", OrderRoutes);
+
+app.listen(3004, () => console.log("Server started at localhost:3004"));
